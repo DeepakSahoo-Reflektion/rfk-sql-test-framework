b5f607f48d72f10c69a2a42bff882619d1515a9e
deepak <deepak.sahoo@reflektion.com>
2018-08-03 13:49:06 +0530

    formatted resolver package

--- engine/resolver/fs_resolver.py
+++ engine/resolver/fs_resolver.py
@@ -1,17 +1,31 @@
 from codecs import open
 import os
+import logging
 
 from engine.resolver.resolver import PathResolver
 
-##TODO: use regex to identify if the file_path ends with .json then read the file or else read all .json files recursively
-##TODO: error and exception handling , return string instead of file
-##TODO: exception handling and logging
-##TODO: take extra argument, so that you can read a single file or which pattern_file to search or all files etc.
+logging.basicConfig(level=logging.DEBUG)
+LOGGER = logging.getLogger()
+
 class FSPathResolver(PathResolver):
+    '''
+    PathResolver implementation of the file system.
+    '''
 
     def resolve(self,file_path):
-        f = open(file_path, 'r', encoding='utf-8')
-        file_string = f.read()
-        f.close()
+        '''
+        Method which will take the file_path as argument and returns the contents.
+        The file_path argument should be an absolute location.
+        :param file_path: string containing file path location
+        :return:
+        '''
+        try:
+            file_string = None
+            f = open(file_path, 'r', encoding='utf-8')
+            file_string = f.read()
+            f.close()
+        except Exception as e:
+            LOGGER.error('Error while reading the file.. %s',e.args)
+            raise e
         return file_string
 
--- engine/resolver/resolver_factory.py
+++ engine/resolver/resolver_factory.py
@@ -1,12 +1,10 @@
 import logging
-from engine.resolver.git_resolver import *
 from engine.resolver.fs_resolver import *
-from engine.resolver.s3_resolver import *
 
 logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger()
+LOGGER = logging.getLogger()
 
-## TODO: improvise the if else condition
+
 class ResolverFactory:
     '''
     Factory for the executor. As of now only one type of executor is supported.
@@ -20,29 +18,22 @@ class ResolverFactory:
 
     @staticmethod
     def get_resolver(type):
+        '''
+        Factory method to return appropriate resolver based on the input type.
+
+        :param type: string
+        :return: Resolver
+        '''
         if not type:
-            logger.error('ExecutorFactory: type is not provided')
+            LOGGER.error('ExecutorFactory: type is not provided')
             return
 
-        if type == 'git':
-            resolver = ResolverFactory._instances.get(type,None)
-            if not resolver:
-                resolver =  GitPathResolver()
-                ResolverFactory._instances[type] = resolver
-            return resolver
-        elif type == 'fs':
+        if type == 'fs':
             resolver = ResolverFactory._instances.get(type, None)
             if not resolver:
                 resolver = FSPathResolver()
                 ResolverFactory._instances[type] = resolver
             return resolver
-        elif type == 's3':
-            resolver = ResolverFactory._instances.get(type, None)
-            if not resolver:
-                resolver = S3PathResolver()
-                ResolverFactory._instances[type] = resolver
-            return resolver
-
         else:
-            logger.warn('ExecutorFactory:get_executor invalid type')
+            LOGGER.warn('ExecutorFactory:get_executor invalid type')
             return None
